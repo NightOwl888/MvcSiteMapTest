1276a0753d1eec313f954da023b2b202f971bba0
tomascassidy <tomas.cassidy@gmail.com>
2013-10-16 12:57:08 +1100

    MvcSiteMapProviderTest

---
+++ MvcSiteMapTest/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using System.Web.Mvc;
+
+namespace MvcSiteMapTest.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        //
+        // GET: /Home/A
+
+        public ActionResult A(int? id)
+        {
+            return View();
+        }
+
+        //
+        // GET: /Home/B
+
+        public ActionResult B(int? id)
+        {
+            return View();
+        }
+
+        //
+        // GET: /Home/C
+
+        public ActionResult C(int? id)
+        {
+            return View();
+        }
+
+    }
+}
